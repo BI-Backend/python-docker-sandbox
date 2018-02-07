ca7257aa1ad2ca68c9746ae41371e3c73353cdfc
Cameron Gray <cameron@camerongray.me>
2018-02-07 23:52:53 +0000

    Moved variables that hold available/running containers to be provided by an instance of multiprocessing.Manager.  Also now only storing container IDs rather than container objects.

--- python_docker_sandbox/pool.py
+++ python_docker_sandbox/pool.py
@@ -4,10 +4,11 @@ import time
 from contextlib import contextmanager
 from collections import deque
 
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Manager
 from queue import Empty
 
 logger = logging.getLogger(__name__)
+manager = Manager()
 
 
 class Pool:
@@ -21,8 +22,8 @@ class Pool:
         self.required_packages = required_packages
         self.base_image = base_image
 
-        self._available_workers = deque([])
-        self._running_workers = {}
+        self._available_workers = manager.list()
+        self._running_workers = manager.dict()
         self.pool_manager_process = None
         self.pool_manager_queue = None
 
@@ -52,15 +53,15 @@ class Pool:
         self.pool_manager_queue.put("STOP")
         self.pool_manager_process.join()
 
-    @contextmanager
-    def get_container(self):
-        # TODO: If pool is empty (could happen if _ensure_minimum_containers hasn't yet run), start a new container
-        container = self._available_workers.popleft()
-        self._running_workers[container.id] = container
-        yield container
-        # We are now finished with the container so stop it
-        del self._running_workers[container.id]
-        container.stop(timeout=self.CONTAINER_STOP_TIMEOUT)
+    # @contextmanager
+    # def get_container(self):
+    #     # TODO: If pool is empty (could happen if _ensure_minimum_containers hasn't yet run), start a new container
+    #     container = self._available_workers.popleft()
+    #     self._running_workers[container.id] = container
+    #     yield container
+    #     # We are now finished with the container so stop it
+    #     del self._running_workers[container.id]
+    #     container.stop(timeout=self.CONTAINER_STOP_TIMEOUT)
 
     def _start_container(self):
         container = self.client.containers.run(self.image_name, auto_remove=True, detach=True)
@@ -84,7 +85,7 @@ class Pool:
 
         for i in range(0, workers_to_start):
             container = self._start_container()
-            self._available_workers.append(container)
+            self._available_workers.append(container.id)
 
         logger.info(f"{len(self._available_workers) + len(self._running_workers)} workers are currently running")
 
@@ -93,7 +94,8 @@ class Pool:
 
         logger.info(f"Shutting down {len(containers_to_stop)} containers")
 
-        for container in containers_to_stop:
+        for container_id in containers_to_stop:
+            container = self.client.containers.get(container_id)
             logger.info(f"Shutting down container {container.id}")
             container.stop(timeout=self.CONTAINER_STOP_TIMEOUT)
 
@@ -101,9 +103,8 @@ class Pool:
         try:
             while True:
                 self._ensure_minimum_containers()
-                time.sleep(0.5)  # TODO: Make this configuraable
                 try:
-                    msg = queue.get(block=False, timeout=None)
+                    msg = queue.get(block=True, timeout=0.5)  # TODO: Make timeout configurable
                     if msg == "STOP":
                         logger.info("Pool manager received stop command, shutting down containers")
                         self._shutdown_all_containers()
